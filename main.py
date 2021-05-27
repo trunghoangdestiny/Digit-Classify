b297c9fc8de6503d48c6d4a862a982a76d21d0cb
ShafiSahal99 <shafisahal99@gmail.com>
2021-05-27 18:34:47 +0530

    Increased epoch count

--- main.py
+++ main.py
@@ -29,5 +29,5 @@ model.compile(
     metrics=['accuracy']
 )
 
-model.fit(x_train, y_train, batch_size=32, epochs=5)
+model.fit(x_train, y_train, batch_size=32, epochs=100)
 model.fit(x_test, y_test, batch_size=32)
