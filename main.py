16af330c87eeff162ea36d2aece2d27a9af22588
ShafiSahal99 <shafisahal99@gmail.com>
2021-05-31 18:55:02 +0530

    Convolutional network

--- main.py
+++ main.py
@@ -3,31 +3,53 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
-from tensorflow.keras.datasets import mnist
+from tensorflow.keras.datasets import cifar10
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train = x_train.reshape(-1, 28 * 28).astype('float32') / 255.0
-x_test = x_test.reshape(-1, 28 * 28).astype('float32') / 255.0
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+x_train = x_train.astype('float32') / 255.0
+x_test = x_test.astype('float32') / 255.0
 
-model = keras.Sequential(
+model1 = keras.Sequential (
     [
-        layers.Dense(512, activation='relu'),
-        layers.Dense(256, activation='relu'),
+        keras.Input(shape=(32, 32, 3)),
+        layers.Conv2D(32, 3, activation='relu'),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Conv2D(64, 3, activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Conv2D(128, 3, activation='relu'),
+        layers.Flatten(),
+        layers.Dense(64, activation='relu'),
         layers.Dense(10)
     ]
 )
 
-inputs = keras.Input(shape=784)
-x = layers.Dense(512, activation='relu')(inputs)
-x = layers.Dense(256, activation='relu')(x)
-outputs = layers.Dense(10, activation='softmax')(x)
-model = keras.Model(inputs=inputs, outputs=outputs)
 
-model.compile(
-    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=False),
-    optimizer=keras.optimizers.Adam(learning_rate=0.001),
+def my_model():
+    inputs = keras.Input(shape=(32, 32, 3))
+    x = layers.Conv2D(32, 3)(inputs)
+    x = layers.BatchNormalization()(x)
+    x = keras.activations.relu(x)
+    x = layers.MaxPooling2D()(x)
+    x = layers.Conv2D(64, 5, padding='same')(x)
+    x = layers.BatchNormalization()(x)
+    x = keras.activations.relu(x)
+    x = layers.Conv2D(128, 3)(x)
+    x = layers.BatchNormalization()(x)
+    x = keras.activations.relu(x)
+    x = layers.Dense(64, activation='relu')(x)
+    x = layers.Flatten()(x)
+    outputs = layers.Dense(10)(x)
+    model = keras.Model(inputs=inputs, outputs=outputs)
+    return model
+
+
+model = my_model()
+
+model.compile (
+    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    optimizer=keras.optimizers.Adam(learning_rate=3e-4),
     metrics=['accuracy']
 )
 
-model.fit(x_train, y_train, batch_size=32, epochs=100)
-model.fit(x_test, y_test, batch_size=32)
+model.fit(x_train, y_train, batch_size=64, epochs=10)
+model.evaluate(x_test, y_test, batch_size=64)
